9dc1f144df4a2ea2fa283bbf6d1564f01faca251
Abdulganiyu-Abdulazeez <abdulganiyuabdulazeez20@gmail.com>
2024-01-04 06:07:55 -0800

    cheapy commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+  import { defineComponent } from 'vue';
+  import navbar from "./components/navbar.vue"
+  import banner from "./components/landing-page.vue"
+  import clothing from "./components/clothings.vue"
+  import jewelery from "./components/jewelery.vue"
+  import electronics from "./components/electronics.vue"
+  import store from "./store/store"
+  export default defineComponent ({
+    components: {navbar, banner, clothing, jewelery, electronics},
+    data() {
+      return {
+        myComponent: 'banner',
+        shop: store().$state.array
+      }
+    },
+    mounted() {
+      fetch('https://fakestoreapi.com/products/').then(response => response.json())
+      .then(data =>{
+        const take = data
+        store().$state.array = take
+        this.shop = take
+        console.log(this.shop)
+      })
+    }
+  })
+</script>
+
+<template>
+  <header class="w-full h-screen bg-blue-300 header overflow-hidden">
+    <navbar @cloth="myComponent='clothing'" @jewelery="myComponent='jewelery'" @electronic="myComponent='electronics'" @logo="myComponent='banner'"/>
+    <!-- <component :is="myComponent" :mall="array"></component> -->
+    <router-view></router-view>
+  </header>
+</template>
+
+<style>
+  .header {
+    width: 100%;
+    height: fit-content;
+    background-color: aliceblue;
+  }
+</style>
---
+++ src/components/landing-page.vue
@@ -0,0 +1,60 @@
+<template>
+<div class="bg-black w-[300vw] h-screen banner relative">
+    <div class="w-[300vw] h-fit flex items-center">
+        <div class="relative w-[100%] h-screen">
+            <div class="w-[100%] h-screen bg-[url('../src/assets/image/cloths3.jpg')] opacity-20 w-[100%] h-screen bg-no-repeat bg-cover absolute"></div>
+            <div class="md:w-[60%] w-[100%] md:text-start mx-auto md:p-[2rem] p-[1rem] md:mx-0 text-center m-[2rem] md:mt-[15rem]">
+                <h1 class="font-[700] lg:text-[60px] md:text-[50px] text-[30px] text-[#7FFF00]">AzedStore is the solution</h1>
+                <p class="font-bold md:text-xl text-[14px] text-white">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, dolor voluptatum repellat distinctio, esse explicabo eaque, ipsa laboriosam blanditiis totam neque. Corporis, itaque! Vero nulla dolore ab, nobis veritatis corrupti.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et laudantium magni libero dolorem voluptatibus commodi beatae voluptas, est amet quidem praesentium reprehenderit ipsam maiores cupiditate, exercitationem odio. Quas, numquam fuga!
+                </p>
+            </div>
+        </div>
+        <div class="relative w-[100%] h-screen">
+            <div class="w-[100%] h-screen bg-[url('../src/assets/image/elctronics1.jpg')] opacity-20 w-[100%] h-screen bg-no-repeat bg-cover absolute"></div>
+            <div class="md:w-[60%] w-[100%] md:text-start mx-auto md:p-[2rem] p-[1rem] md:mx-0 text-center m-[2rem] md:mt-[15rem]">
+                <h1 class="font-[700] lg:text-[60px] md:text-[50px] text-[30px] text-[#7FFF00]">AzedStore is the solution</h1>
+                <p class="font-bold md:text-xl text-[14px] text-white">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, dolor voluptatum repellat distinctio, esse explicabo eaque, ipsa laboriosam blanditiis totam neque. Corporis, itaque! Vero nulla dolore ab, nobis veritatis corrupti.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et laudantium magni libero dolorem voluptatibus commodi beatae voluptas, est amet quidem praesentium reprehenderit ipsam maiores cupiditate, exercitationem odio. Quas, numquam fuga!
+                </p>
+            </div>
+        </div>
+        <div class="relative w-[100%] h-screen">
+            <div class="w-[100%] h-screen bg-[url('../src/assets/image/cloth2.jpg')] opacity-20 w-[100%] h-screen bg-no-repeat bg-cover absolute"></div>
+            <div class="md:w-[60%] w-[100%] md:text-start mx-auto md:p-[2rem] p-[1rem] md:mx-0 text-center m-[2rem] md:mt-[15rem]">
+                <h1 class="font-[700] lg:text-[60px] md:text-[50px] text-[30px] text-[#7FFF00]">AzedStore is the solution</h1>
+                <p class="font-bold md:text-xl text-[14px] text-white">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, dolor voluptatum repellat distinctio, esse explicabo eaque, ipsa laboriosam blanditiis totam neque. Corporis, itaque! Vero nulla dolore ab, nobis veritatis corrupti.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et laudantium magni libero dolorem voluptatibus commodi beatae voluptas, est amet quidem praesentium reprehenderit ipsam maiores cupiditate, exercitationem odio. Quas, numquam fuga!
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+<style>
+    .banner {
+        animation-name: anime;
+        animation-delay: 2s;
+        animation-duration: 20s;
+        animation-timing-function: ease;
+        animation-iteration-count: infinite;
+        animation-direction: alternate;
+    }
+    @keyframes anime {
+        0% {
+            top: 0;
+            left: 0vw;
+        }
+        50% {
+            top: 0;
+            left: -100vw;
+        }
+        100% {
+            top: 0;
+            left: -200vw;
+        }
+    }
+</style>
